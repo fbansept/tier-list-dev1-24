d21a2d841ee2c52bb53d5609aa46ee51496caad5
fbansept <bansept.franck@gmail.com>
2024-05-24 11:48:04 +0200

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ListeComponent } from "./liste/liste.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [ListeComponent]
 })
 export class AppComponent {
-  title = 'tier-list-dev1-24';
+
 }
---
+++ src/app/liste/liste.component.ts
@@ -0,0 +1,75 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-liste',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './liste.component.html',
+  styleUrl: './liste.component.scss',
+})
+export class ListeComponent {
+  listeCategories: { nom: string; elements: string[] }[] = [];
+  urlElementSaisie: string = '';
+
+  ngOnInit() {
+    const donnees = localStorage.getItem('listeCategories');
+
+    if (donnees != null) {
+      this.listeCategories = JSON.parse(donnees);
+    } else {
+      this.reset();
+    }
+  }
+
+  reset() {
+    this.listeCategories = [
+      { nom: 'Top', elements: [] },
+      { nom: 'Bon', elements: [] },
+      { nom: 'Moyen', elements: [] },
+      { nom: 'Mauvais', elements: [] },
+      { nom: 'Horrible', elements: [] },
+    ];
+    this.sauvegarde();
+  }
+
+  onAjoutElement() {
+    this.listeCategories[0].elements.push(this.urlElementSaisie);
+    this.urlElementSaisie = '';
+
+    this.sauvegarde();
+  }
+
+  onSuppressionImage(indexCategorie: number, indexElement: number) {
+    this.listeCategories[indexCategorie].elements.splice(indexElement, 1);
+
+    this.sauvegarde();
+  }
+
+  onChangementCategorieImage(
+    indexCategorie: number,
+    indexElement: number,
+    augmente: boolean
+  ) {
+    //on recupère la catégorie où l'élément doit etre déplacé
+    const nouvelleCategorie =
+      this.listeCategories[augmente ? indexCategorie - 1 : indexCategorie + 1];
+
+    //on copie l'élément déplacé dans la nouvelle catégorie
+    nouvelleCategorie.elements.push(
+      this.listeCategories[indexCategorie].elements[indexElement]
+    );
+
+    //on supprime l'élément déplacé de son anciennne catégorie
+    this.listeCategories[indexCategorie].elements.splice(indexElement, 1);
+
+    this.sauvegarde();
+  }
+
+  sauvegarde() {
+    localStorage.setItem(
+      'listeCategories',
+      JSON.stringify(this.listeCategories)
+    );
+  }
+}
